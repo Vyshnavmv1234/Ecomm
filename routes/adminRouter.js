8b720006a4fac51b06dadd81d7b8d0b0b51e56c1
Vyshnav Mv <vyshnavmv92295@gmail.com>
2026-03-16 02:10:52 +0530

    product add issue

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -32,7 +32,7 @@ router.patch("/users/:id/unblock",adminAuth,customerController.customerUnBlock)
 
 router.get("/products",adminAuth,productController.loadProducts)
 router.get("/addProducts",adminAuth,productController.loadAddProducts)
-router.post("/addProduct",uploadProduct.array("images", 5),adminAuth,productController.postAddProducts)
+router.post("/addProduct",adminAuth,uploadProduct.array("images", 5),productController.postAddProducts)
 
 router.get("/editProduct/:id",adminAuth,productController.loadEditProduct)
 router.patch("/blockProduct/:id",adminAuth,productController.blockProduct)
